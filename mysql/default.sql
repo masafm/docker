f0a24e50f6c2cb04cbeb9d8682034b4a7092170f
masafm <masafumi.kashiwagi@datadoghq.com>
2023-03-28 14:08:05 +0900

    update

--- mysql/default.sql
+++ mysql/default.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE wordpress;
 CREATE USER wordpress IDENTIFIED BY 'wordpress';
 GRANT ALL PRIVILEGES ON wordpress.* TO wordpress;
+CREATE USER 'datadog'@'%' IDENTIFIED WITH mysql_native_password by 'datadog';
+ 
