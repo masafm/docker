f7b3f0bf2bc8b59fdfca1aeb86739bbabe4c22b7
Masafumi Kashiwagi <mkashi3@gmail.com>
2023-08-11 19:51:03 +0900

    update

--- selenium/entrypoint.sh
+++ selenium/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISPLAY=:1.0
 vncserver :1 -geometry 1280x1200
-./Selenium-Automation/sbi.py --password2="${PASSWORD2}" "${KEY}" "${USER}" "${PASSWORD}"
+./Selenium-Automation/sbi.py --password2="${SBI_PASSWORD2}" "${SELENIUM_KEY}" "${SBI_USER}" "${SBI_PASSWORD}"
