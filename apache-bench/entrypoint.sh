38caa614ffae8a82e027e1c4146c1bf02fc09ec7
masafm <masafumi.kashiwagi@datadoghq.com>
2023-08-14 12:51:34 +0900

    update

---
+++ apache-bench/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+c=4
+n=10000
+
+if [ -n "${AB_CONCURRENCY}" ];then
+    c=${AB_CONCURRENCY}
+fi
+if [ -n "${AB_REQUESTS}" ];then
+    n=${AB_REQUESTS}
+fi
+
+while true;do
+    cmd="ab -c $c -n $n http://localhost/"
+    echo "Start $cmd"
+    $cmd
+    echo "Done $cmd"
+done
