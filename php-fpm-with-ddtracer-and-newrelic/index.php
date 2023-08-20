e97f94511fe4889a6bd1b1081b6237d243f9cc06
masafm <masa@mkashi.com>
2023-08-20 04:46:58 +0000

    update

--- php-fpm-with-ddtracer-and-newrelic/index.php
+++ php-fpm-with-ddtracer-and-newrelic/index.php
@@ -2,7 +2,7 @@
 for ($i = 1; $i <= 10; $i++) {
     // Access to Nginx status page
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://localhost:81/");
+    curl_setopt($ch, CURLOPT_URL, "http://localhost:81/nginx_status");
     $res =  curl_exec($ch);
     curl_close($ch);
     var_dump($res);
