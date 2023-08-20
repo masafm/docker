083ab63d96f6cd64059fb58b72f43d5b57c25ea7
masafm <masa@mkashi.com>
2023-08-20 05:14:40 +0000

    update

--- php-fpm-without-tracer/index.php
+++ php-fpm-without-tracer/index.php
@@ -1,5 +1,7 @@
 <?php
-for ($i = 1; $i <= 10; $i++) {
+$count = getenv('CURL_COUNT');
+$count = $count ? $count : 10;
+for ($i = 1; $i <= $count; $i++) {
     // Access to Nginx status page
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "http://localhost:81/nginx_status");
