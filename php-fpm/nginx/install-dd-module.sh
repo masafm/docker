0022909a83804f945a44a4bc849c1645cd753fd3
masafm <masa@mkashi.com>
2023-09-14 23:08:43 +0900

    update

---
+++ php-fpm/nginx/install-dd-module.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | jq --raw-output .tag_name
+}
+BASE_IMAGE=$1
+BASE_IMAGE_WITHOUT_COLONS=$(echo "$BASE_IMAGE" | tr ':' '_')
+RELEASE_TAG=$(get_latest_release DataDog/nginx-datadog)
+tarball="$BASE_IMAGE_WITHOUT_COLONS-ngx_http_datadog_module.so.tgz"
+wget "https://github.com/DataDog/nginx-datadog/releases/download/$RELEASE_TAG/$tarball"
+tar -xzf "$tarball" -C /usr/lib/nginx/modules
+rm "$tarball"
+ls -l /usr/lib/nginx/modules/ngx_http_datadog_module.so
