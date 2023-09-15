5adce59ecf8f51a2c67dc150d64ee53cf6bc1855
masafm <masa@mkashi.com>
2023-09-15 19:41:02 +0900

    update

--- php-fpm/index.php
+++ php-fpm/index.php
@@ -1,1 +1,12 @@
-<?php phpinfo(); ?>
+<?php
+$count = getenv('CURL_COUNT');
+$count = $count ? $count : 10;
+for ($i = 1; $i <= $count; $i++) {
+    // Access to Nginx status page
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://localhost:81/nginx_status");
+    $res =  curl_exec($ch);
+    curl_close($ch);
+    var_dump($res);
+}
+?>
