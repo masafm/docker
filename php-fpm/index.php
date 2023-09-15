bd85cc6f942a9e44b441d791c6caca67c7eef1c6
masafm <masa@mkashi.com>
2023-09-15 20:30:40 +0900

    update

--- php-fpm/index.php
+++ php-fpm/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo(); ?>
+<?php
+$count = getenv('CURL_COUNT');
+$count = $count ? $count : 10;
+for ($i = 1; $i <= $count; $i++) {
+    // Access to Nginx status page
+    $ch = curl_init();
+    //curl_setopt($ch, CURLOPT_URL, "http://localhost:81/nginx_status");
+    curl_setopt($ch, CURLOPT_URL, "https://jlbwv05btk.execute-api.ap-northeast-1.amazonaws.com/default/masafumi-kashiwagi-function2");
+    $res =  curl_exec($ch);
+    curl_close($ch);
+    var_dump($res);
+}
+?>
