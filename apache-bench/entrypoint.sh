67db1f6ff2e51cf0d0c0fa9dc35c074ea501d3a5
masafm <masa@mkashi.com>
2023-09-16 21:04:28 +0900

    update

--- apache-bench/entrypoint.sh
+++ apache-bench/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 c=4
 n=10000
+t=localhost
 
 if [ -n "${AB_CONCURRENCY}" ];then
     c=${AB_CONCURRENCY}
@@ -8,9 +9,12 @@ fi
 if [ -n "${AB_REQUESTS}" ];then
     n=${AB_REQUESTS}
 fi
+if [ -n "${AB_TARGET}" ];then
+    t=${AB_TARGET}
+fi
 
 while true;do
-    cmd="ab -c $c -n $n http://nginx/"
+    cmd="ab -c $c -n $n http://${t}/"
     echo "Start $cmd"
     $cmd
     echo "Done $cmd"
