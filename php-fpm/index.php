fbffde6f2e360a5105d06f14acae08e816297666
masafm <masa@mkashi.com>
2023-09-17 12:59:38 +0900

    update

--- php-fpm/index.php
+++ php-fpm/index.php
@@ -1,11 +1,11 @@
 <?php
 $count = getenv('CURL_COUNT');
+$url = getenv('CURL_TARGET_URL') ? getenv('CURL_TARGET_URL') : 'http://localhost:81/nginx_status';
 $count = $count ? $count : 10;
 for ($i = 1; $i <= $count; $i++) {
     // Access to Nginx status page
     $ch = curl_init();
-    //curl_setopt($ch, CURLOPT_URL, "http://localhost:81/nginx_status");
-    curl_setopt($ch, CURLOPT_URL, "https://o8oxu72lo3.execute-api.ap-northeast-1.amazonaws.com/default");//Private API GW
+    curl_setopt($ch, CURLOPT_URL, $url);
     $res =  curl_exec($ch);
     curl_close($ch);
     var_dump($res);
