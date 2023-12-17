61be437ae08dbfe78163c52c7d1a3b396decc477
masafm <masa@mkashi.com>
2023-12-17 13:17:37 +0900

    update

--- java/run.sh
+++ java/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
+if [[ $1 == clean ]];then
+    rm -rf src dd-java-agent.jar
+fi
+
 if [[ ! -d src ]];then
-    (git clone git@github.com:masafm/java.git src; cd src/jmx-test; git checkout a6c17998b6255822f541c4ddb4906f1447bff97f)
+    (git clone https://github.com/masafm/java.git src; cd src/jmx-test; git checkout aeedbda889ba83a90a3879ffec53df75d5220fec)
 fi
 
 if [[ ! -f dd-java-agent.jar ]];then
    curl -o dd-java-agent.jar https://repo1.maven.org/maven2/com/datadoghq/dd-java-agent/1.25.1/dd-java-agent-1.25.1.jar
 fi
 
-for task in clean build run;do
-    echo "Running task name:${task}"
-    docker run --rm -u gradle -v "${PWD}/src/jmx-test":/home/gradle/project -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" -w /home/gradle/project --network datadog -e DD_AGENT_HOST=datadog-agent -e DD_SERVICE=jmx-test -e DD_SOURCE=java -e DD_VERSION=1.0 -l com.datadoghq.ad.logs="[{\"source\": \"java\", \"service\": \"jmx-test\"}]" gradle:8.5.0-jdk17 gradle $task
-    if [[ $? != 0 ]];then
-	echo "task:${task} failed!"
-	break;
-    fi
-done
+docker run --rm -u gradle -v "${PWD}/src/jmx-test":/home/gradle/project -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" -w /home/gradle/project --network datadog -e DD_AGENT_HOST=datadog-agent -e TARGET_URL=http://www.datadoghq.com/ -l com.datadoghq.ad.logs="[{\"source\": \"java\", \"service\": \"jmx-test\"}]" gradle:8.5.0-jdk17-focal gradle build run
