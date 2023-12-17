d45f41b1c7171646d05e5cce3bc5639ab965397e
masafm <masa@mkashi.com>
2023-12-18 07:16:11 +0900

    update

--- java/run.sh
+++ java/run.sh
@@ -5,11 +5,19 @@ if [[ $1 == clean ]];then
 fi
 
 if [[ ! -d src ]];then
-    (git clone https://github.com/masafm/java.git src; cd src/jmx-test; git checkout aeedbda889ba83a90a3879ffec53df75d5220fec)
+    (git clone https://github.com/masafm/java.git src; cd src/jmx-test; git checkout gradle)
 fi
 
 if [[ ! -f dd-java-agent.jar ]];then
    curl -o dd-java-agent.jar https://repo1.maven.org/maven2/com/datadoghq/dd-java-agent/1.25.1/dd-java-agent-1.25.1.jar
 fi
 
-docker run --rm -u gradle -v "${PWD}/src/jmx-test":/home/gradle/project -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" -w /home/gradle/project --network datadog -e DD_AGENT_HOST=datadog-agent -e TARGET_URL=http://www.datadoghq.com/ -l com.datadoghq.ad.logs="[{\"source\": \"java\", \"service\": \"jmx-test\"}]" gradle:8.5.0-jdk17-focal gradle build run
+docker run --rm -u gradle \
+       -v "${PWD}/src/jmx-test":/home/gradle/project \
+       -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" \
+       -w /home/gradle/project \
+       --network datadog \
+       -e DD_AGENT_HOST=datadog-agent \
+       -l com.datadoghq.ad.logs="[{\"source\": \"java\", \"service\": \"jmx-test\"}]" \
+       gradle:8.5.0-jdk17-focal \
+       gradle build run
