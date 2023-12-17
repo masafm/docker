4e852fd08764f547595ea2ad49a0a532e12e1878
masafm <masa@mkashi.com>
2023-12-17 12:02:36 +0900

    update

--- java/run.sh
+++ java/run.sh
@@ -10,7 +10,7 @@ fi
 
 for task in clean build run;do
     echo "Running task name:${task}"
-    docker run --rm -u gradle -v "${PWD}/src/jmx-test":/home/gradle/project -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" -w /home/gradle/project gradle:8.5.0-jdk17 gradle $task
+    docker run --rm -u gradle -v "${PWD}/src/jmx-test":/home/gradle/project -v "${PWD}/dd-java-agent.jar:/opt/dd-java-agent.jar" -w /home/gradle/project --network datadog -e DD_AGENT_HOST=datadog-agent -e DD_SERVICE=jmx-test -e DD_SOURCE=java -e DD_VERSION=1.0 -l com.datadoghq.ad.logs="[{\"source\": \"java\", \"service\": \"jmx-test\"}]" gradle:8.5.0-jdk17 gradle $task
     if [[ $? != 0 ]];then
 	echo "task:${task} failed!"
 	break;
