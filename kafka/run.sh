f6f8a75c7e048ff2c64c972a9363007f5f2b37e4
masafm <masa@mkashi.com>
2024-01-18 07:26:01 +0900

    update

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
-export KAFKA_ZOOKEEPER_CONNECT=zookeeper:32181
-export KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092
-
-docker exec -it kafka_kafka_1 kafka-topics --create --bootstrap-server localhost:9092 --topic quickstart-events1 --partitions 100 --replication-factor 1
-docker exec -it kafka_kafka_1 kafka-topics --create --bootstrap-server localhost:9092 --topic quickstart-events2 --partitions 100 --replication-factor 1
-docker exec -it kafka_kafka_1 kafka-topics --create --bootstrap-server localhost:9092 --topic quickstart-events3 --partitions 100 --replication-factor 1
-docker exec -it kafka_kafka_1 kafka-topics --create --bootstrap-server localhost:9092 --topic quickstart-events4 --partitions 100 --replication-factor 1
-
-nohup docker exec kafka_kafka_1 sh -c "yes | kafka-console-producer --topic quickstart-events1 --bootstrap-server localhost:9092" >/dev/null 2>&1 &
-nohup docker exec kafka_kafka_1 sh -c "yes | kafka-console-producer --topic quickstart-events2 --bootstrap-server localhost:9092" >/dev/null 2>&1 &
-nohup docker exec kafka_kafka_1 sh -c "yes | kafka-console-producer --topic quickstart-events3 --bootstrap-server localhost:9092" >/dev/null 2>&1 &
-nohup docker exec kafka_kafka_1 sh -c "yes | kafka-console-producer --topic quickstart-events4 --bootstrap-server localhost:9092" >/dev/null 2>&1 &
-
-nohup docker exec -it kafka_kafka_1 sh -c "kafka-console-consumer --topic quickstart-events1 --bootstrap-server localhost:9092 --from-beginning --group consumer-group1" >/dev/null 2>&1 &
-nohup docker exec -it kafka_kafka_1 sh -c "kafka-console-consumer --topic quickstart-events2 --bootstrap-server localhost:9092 --from-beginning --group consumer-group2" >/dev/null 2>&1 &
-nohup docker exec -it kafka_kafka_1 sh -c "kafka-console-consumer --topic quickstart-events3 --bootstrap-server localhost:9092 --from-beginning --group consumer-group3" >/dev/null 2>&1 &
-nohup docker exec -it kafka_kafka_1 sh -c "kafka-console-consumer --topic quickstart-events4 --bootstrap-server localhost:9092 --from-beginning --group consumer-group4" >/dev/null 2>&1 &
-
+docker exec -it kafka kafka-consumer-groups --bootstrap-server kafka:9092 --list
+docker exec -it kafka kafka-consumer-groups --bootstrap-server kafka:9092 --group consumer-group1 --describe
