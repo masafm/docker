6d810784a2104d2adefdd723b4b38d9739df3b8d
masafm <masa@mkashi.com>
2024-08-21 15:22:01 +0900

    update

--- php-fpm/nginx/install-dd-module.sh
+++ php-fpm/nginx/install-dd-module.sh
@@ -6,7 +6,8 @@ get_latest_release() {
 BASE_IMAGE=$1
 BASE_IMAGE_WITHOUT_COLONS=$(echo "$BASE_IMAGE" | tr ':' '_')
 RELEASE_TAG=$(get_latest_release DataDog/nginx-datadog)
-tarball="$BASE_IMAGE_WITHOUT_COLONS-amd64-ngx_http_datadog_module.so.tgz"
+#tarball="$BASE_IMAGE_WITHOUT_COLONS-amd64-ngx_http_datadog_module.so.tgz"
+tarball="ngx_http_datadog_module-amd64-1.27.0.so.tgz"
 wget "https://github.com/DataDog/nginx-datadog/releases/download/$RELEASE_TAG/$tarball"
 tar -xzf "$tarball" -C /usr/lib/nginx/modules
 rm "$tarball"
