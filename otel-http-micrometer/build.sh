6b32a537230a7d1529aaeebbba76ec8a92034d67
masafm <masa@mkashi.com>
2024-08-26 16:51:35 +0900

    update

--- otel-http-micrometer/build.sh
+++ otel-http-micrometer/build.sh
@@ -1,7 +1,9 @@
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 dd_trace_lib=https://repo1.maven.org/maven2/com/datadoghq/dd-java-agent/1.38.1/dd-java-agent-1.38.1.jar
+mvn=/opt/apache-maven-3.9.9/bin/mvn
 for d in httpservice1-micrometer httpservice2-micrometer; do
     wget -O $d/dd-java-agent.jar $dd_trace_lib
 done
-JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64 /opt/apache-maven-3.9.9/bin/mvn clean
-JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64 /opt/apache-maven-3.9.9/bin/mvn spring-boot:build-image
+$mvn clean
+$mvn spring-boot:build-image
 docker compose build
