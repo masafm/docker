3ee18353244d794b89558c71469cf86317b60174
masafm <masa@mkashi.com>
2025-05-09 05:05:57 +0000

    update

--- sqlserver/datadog/setup.sh
+++ sqlserver/datadog/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10
-/opt/mssql-tools/bin/sqlcmd -S sqlserver -U SA -P "${MSSQL_SA_PASSWORD}" -i /datadog/datadog.sql
+/opt/mssql-tools18/bin/sqlcmd -S sqlserver -U SA -P "${MSSQL_SA_PASSWORD}" -i /datadog/datadog.sql -C
